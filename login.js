d6779d54a022750aaa324e01598c6ba05de36874
wynn-stan <93167881+wynn-stan@users.noreply.github.com>
2022-08-08 15:48:27 +0000

    Create login.js

---
+++ login.js
@@ -0,0 +1,9 @@
+/*
+  user sends login info
+  we check database to see if valid
+  if yes, generate a jwt token
+  send jwt token to user as part of response
+  
+  NOTE: for every subsequent action check if user has a valid jwt. 
+  
+*/
