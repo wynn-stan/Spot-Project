ae9e87e8d6c35452d50125791cdc50e793ba8eb1
wynn-stan <93167881+wynn-stan@users.noreply.github.com>
2022-09-26 17:12:01 +0000

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-/*
-  user sends login info
-  we check database to see if valid
-  if yes, generate a jwt token
-  send jwt token to user as part of response
-  
-  NOTE: for every subsequent action check if user has a valid jwt. 
-  
-*/
